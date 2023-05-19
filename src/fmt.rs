9455e1316333cc5c52f91f6c9ed290f3ec707a57
yuyang <yuyangxxxmail@gmail.com>
2023-05-19 11:18:16 +0800

    update deps

--- src/fmt.rs
+++ src/fmt.rs
@@ -1,29 +1,43 @@
 #![allow(dead_code)]
 
-use std::path::Path;
+use std::{cell::RefCell, path::Path};
 
-use cranelift_isle::ast::*;
+use cranelift_isle::{ast::*, error::Errors, lexer::Lexer, parser};
 
-fn fmt(_path: impl AsRef<Path>) -> String {
+fn fmt(path: impl AsRef<Path>) -> Result<String, Errors> {
+    let path = path.as_ref();
+    let lexer = Lexer::from_files(vec![path])?;
+
+    let ast = parser::parse(lexer)?;
+
     unimplemented!()
 }
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
 struct Fmt {
-    result: String,
+    result: RefCell<String>,
+    d: Vec<Def>,
 }
 
 impl Fmt {
-    fn new() -> Self {
-        Self::default()
-    }
-    fn format_defs(&mut self, d: &Vec<Def>) {
-        for d in d.iter() {
-            self.format_def(d);
+    fn new(d: Vec<Def>) -> Self {
+        Self {
+            result: Default::default(),
+            d,
         }
     }
+    fn new_line(&self) {
+        unimplemented!()
+    }
+    fn format_defs(self) -> String {
+        for d in self.d.iter() {
+            self.format_def(d);
+            self.new_line();
+        }
+        self.result.into_inner()
+    }
 
-    fn format_def(&mut self, d: &Def) {
+    fn format_def(&self, d: &Def) {
         match d {
             Def::Pragma(x) => self.format_pragma(x),
             Def::Type(x) => self.format_type(x),
@@ -34,53 +48,173 @@ impl Fmt {
             Def::Converter(x) => self.format_converter(x),
         }
     }
-    fn format_pragma(&mut self, _d: &Pragma) {
+    fn format_pragma(&self, _d: &Pragma) {
         // nothing here.
+        unreachable!("rightnow ISLE has no Pragma")
     }
-    fn format_type(&mut self, d: &Type) {
+    fn format_type(&self, d: &Type) {
         //
-        self.result.push_str(format!("(type {}", d.name.0).as_str());
+        self.push_str(format!("(type {}", d.name.0).as_str());
         if d.is_extern {
-            self.result.push_str(" extern");
-        }
-        if d.is_nodebug {
-            self.result.push_str(" nodebug");
+            self.push_str(" extern");
         }
 
+        if d.is_nodebug {
+            self.push_str(" nodebug");
+        }
         match &d.ty {
-            TypeValue::Primitive(name, _) => self.result.push_str(name.0.as_str()),
+            TypeValue::Primitive(name, _) => self.push_str(name.0.as_str()),
             TypeValue::Enum(vs, _) => {
-                self.result
-                    .push_str(format!("\n{}(enum\n", Self::ident(1)).as_str());
+                self.push_str(format!("\n{}(enum\n", Self::ident(1)).as_str());
                 for v in vs.iter() {
-                    self.result
-                        .push_str(format!("{}{}(\n", Self::ident(1), v.name.0,).as_str());
+                    self.push_str(format!("{}{}(\n", Self::ident(1), v.name.0,).as_str());
                     for f in v.fields.iter() {
-                        self.result.push_str(
+                        self.push_str(
                             format!("{}({} {})\n", Self::ident(2), f.name.0, f.ty.0).as_str(),
                         );
                     }
-                    self.result
-                        .push_str(format!("{})\n", Self::ident(1)).as_str());
+                    self.push_str(format!("{})\n", Self::ident(1)).as_str());
                 }
-                self.result.push_str(")");
+                self.push_str(")");
             }
         }
-        self.result.push_str(")\n");
+        self.push_str(")\n");
     }
-    fn format_rule(&mut self, _d: &Rule) {}
-    fn format_extractor(&mut self, _d: &Extractor) {}
-    fn format_decl(&mut self, _d: &Decl) {}
-    fn format_extern(&mut self, _d: &Extern) {}
-    fn format_converter(&mut self, _d: &Converter) {}
+
+    fn format_rule(&self, r: &Rule) {
+        self.format_pattern(&r.pattern);
+    }
+    fn format_pattern(&self, p: &Pattern) {
+        match p {
+            Pattern::Var { var, pos } => self.push_str(&var.0),
+            Pattern::BindPattern { var, subpat, pos } => {
+                self.push_str("(");
+                self.push_str(var.0.as_str());
+                self.push_str(" ");
+
+                self.push_str(")");
+            }
+            Pattern::ConstInt { val, pos } => todo!(),
+            Pattern::ConstPrim { val, pos } => todo!(),
+            Pattern::Term { sym, args, pos } => todo!(),
+            Pattern::Wildcard { pos } => todo!(),
+            Pattern::And { subpats, pos } => todo!(),
+            Pattern::MacroArg { index, pos } => todo!(),
+        }
+    }
+    fn format_extractor(&self, e: &Extractor) {
+        self.push_str("(extractor ");
+        {
+            // pattern
+            self.push_str("(");
+            self.push_str(&e.term.0);
+            self.push_str(" ");
+            let last = e.args.len() - 1;
+            for (index, e) in e.args.iter().enumerate() {
+                self.push_str(&e.0);
+                if index != last {
+                    self.push_str(" ");
+                }
+            }
+            self.push_str(")");
+        }
+        self.format_pattern(&e.template);
+        self.push_str(")");
+    }
+    fn format_decl(&self, d: &Decl) {
+        self.push_str("(rule ");
+        if d.pure {
+            self.push_str("pure ");
+        }
+        if d.multi {
+            self.push_str("multi ");
+        }
+        if d.partial {
+            self.push_str("partial ");
+        }
+        self.push_str(d.term.0.as_str());
+        self.push_str(" ");
+        {
+            //args
+            self.push_str("(");
+            if d.arg_tys.len() > 0 {
+                let last = d.arg_tys.len() - 1;
+                for (index, a) in d.arg_tys.iter().enumerate() {
+                    self.push_str(a.0.as_str());
+                    if index != last {
+                        self.push_str(",");
+                    }
+                }
+            }
+            self.push_str(")");
+        }
+        self.push_str(" ");
+        self.push_str(d.ret_ty.0.as_str());
+        self.push_str(")");
+    }
+
+    fn format_extern(&self, e: &Extern) {
+        self.push_str("(extern ");
+        match e {
+            Extern::Extractor {
+                term,
+                func,
+                pos,
+                infallible,
+            } => {
+                if *infallible {
+                    self.push_str("infallible ");
+                }
+                self.push_str("extractor ");
+                self.push_str(term.0.as_str());
+                self.push_str(" ");
+                self.push_str(func.0.as_str());
+            }
+
+            Extern::Constructor { term, func, pos } => {
+                self.push_str("constructor ");
+                self.push_str(term.0.as_str());
+                self.push_str(" ");
+                self.push_str(func.0.as_str());
+            }
+            Extern::Const { name, ty, pos } => {
+                self.push_str(name.0.as_str());
+                self.push_str(" ");
+                self.push_str(ty.0.as_str());
+            }
+        }
+        self.push_str(")");
+    }
+    fn format_converter(&self, c: &Converter) {
+        self.push_str("(convertor ");
+        self.push_str(c.term.0.as_str());
+        self.push_str(" ");
+        self.push_str(c.inner_ty.0.as_str());
+        self.push_str(" ");
+        self.push_str(c.outer_ty.0.as_str());
+        self.push_str(")");
+    }
 
     fn ident(n: usize) -> String {
         "  ".to_string().repeat(n)
     }
+
+    fn push_str(&self, s: impl AsRef<str>) {
+        let s = s.as_ref();
+        self.result.borrow_mut().push_str(s);
+    }
+    fn last_line_length(&self) -> usize {
+        self.result
+            .borrow()
+            .lines()
+            .last()
+            .map(|x| x.len())
+            .unwrap_or(0)
+    }
 }
 
 #[test]
-fn test_enum() {
+fn test_fmt() {
     use cranelift_isle::lexer::Lexer;
     use cranelift_isle::parser::parse;
     let l = Lexer::from_str(
@@ -98,7 +232,7 @@ fn test_enum() {
     )
     .unwrap();
     let a = parse(l).unwrap();
-    let mut f = Fmt::new();
-    f.format_defs(&a.defs);
-    eprintln!("{}", f.result);
+    let mut f = Fmt::new(a.defs);
+    let result = f.format_defs();
+    eprintln!("{}", result);
 }
